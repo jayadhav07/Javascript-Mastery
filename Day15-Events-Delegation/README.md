f1fbbaafd3f11da5d9594de75be38bf1e49df991
jayadhav07 <jayadhav786@gmail.com>
2025-10-05 18:50:29 +0530

    update event delegation

--- Day15-Events-Delegation/README.md
+++ Day15-Events-Delegation/README.md
@@ -1,14 +1,26 @@
 # 📘 Day 15 – Events & Delegation
 
 ## 📖 Topics
+
 - addEventListener
-- Event bubbling, delegation
+- Event bubbling, delegation,capturing, event.target, event.currentTarget, propgation,
+- Hot to stop capturing and bubbling
+
+=> when and in which derection the event will be executed is known as event propagation
+=> In event bubbling events are executed from bottom to top
+=> event.currentTarget and event.this follows the bubbling concept but event.target does not
+=> In event capturing events are executed from top to bottom
+=> we can stop capturing and bubbling using event.stopPropagation()
+=> In event delegation we add the event to parent element instead of descedents (children) element
 
 ## 👨‍💻 Practice
+
 - Add dynamic event handling in Todo
 
 ## 🚀 Mini Task
+
 Implement event delegation for delete buttons.
 
 ## 🧠 Summary
+
 - Delegation optimizes event management.
--- Day15-Events-Delegation/index.js
+++ Day15-Events-Delegation/index.js
@@ -5,4 +5,86 @@
 //     e.target.closest('li').remove();
 //   }
 // });
-console.log('Event delegation example provided as browser snippet.');
+console.log("Event delegation example provided as browser snippet.");
+
+// Event Bubbling eg.
+
+// const div = document.querySelector("div");
+// const form = document.querySelector("form");
+// const button = document.querySelector("button");
+
+// div.addEventListener("click", () => {
+//   alert("div");
+// });
+// form.addEventListener("click", () => {
+//   alert("form");
+// });
+
+// button.addEventListener("click", () => {
+//   alert("button");
+// });
+
+// DIfferent between event.currenttarget and event.target
+
+// const div = document.querySelector("div");
+// const form = document.querySelector("form");
+// const button = document.querySelector("button");
+
+// div.addEventListener("click", func);
+// form.addEventListener("click", func);
+
+// button.addEventListener("click", func);
+
+// function func() {
+//   alert(
+//     "currentTarget = " +
+//       event.currentTarget.tagName +
+//       ", target = " +
+//       event.target.tagName +
+//       ", this = " +
+//       this.tagName
+//   );
+// }
+
+// what is event capturing
+
+// const div = document.querySelector("div");
+// const form = document.querySelector("form");
+// const button = document.querySelector("button");
+
+// div.addEventListener(
+//   "click",
+//   () => {
+//     alert("div");
+//   },
+//   {
+//     capture: true,
+//   }
+// );
+// form.addEventListener(
+//   "click",
+//   () => {
+//     alert("form");
+//   },
+//   {
+//     capture: true,
+//   }
+// );
+
+// button.addEventListener(
+//   "click",
+//   () => {
+//     alert("button");
+//   },
+//   {
+//     capture: true,
+//   }
+// );
+
+// event delegation
+document.querySelector(".products").addEventListener("click", (e) => {
+  console.log(e.target.tagName);
+  if (e.target.tagName === "SPAN") {
+    window.location.href += "/" + e.target.className;
+  }
+});
